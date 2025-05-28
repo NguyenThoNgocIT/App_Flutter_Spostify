c78802e9dd797d2175a92a553361ea8b34bf811b
nguyentran344 <nguyentran12022@gmail.com>
2025-05-28 11:20:37 +0700

    thêm podcast

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -10,13 +10,9 @@ import 'package:spotify/domain/usecases/song/get_news_songs.dart';
 import 'package:spotify/domain/usecases/song/get_play_list.dart';
 import 'package:spotify/domain/usecases/song/is_favorite_song.dart';
 import 'package:spotify/domain/usecases/podcast/get_podcasts.dart';
-
 import 'data/repository/song/song_repository_impl.dart';
 import 'data/sources/song/song_supabase_service.dart';
 import 'domain/repository/song/song.dart';
-
-import 'data/repository/podcast/podcast_repository_impl.dart';
-import 'data/sources/podcast/podcast_supabase_service.dart';
 import 'domain/repository/podcast/podcast.dart';
 import 'domain/usecases/auth/sigin.dart';
 
@@ -45,7 +41,7 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<GetFavoriteSongsUseCase>(GetFavoriteSongsUseCase());
 
 // Register podcast use cases
-  sl.registerSingleton<PodcastSupabaseService>(PodcastSupabaseServiceImpl());
-  sl.registerSingleton<PodcastsRepository>(PodcastRepositoryImpl());
+  sl.registerLazySingleton<PodcastsRepository>(
+      () => sl<SongsRepository>() as SongRepositoryImpl);
   sl.registerSingleton<GetPodcastsUseCase>(GetPodcastsUseCase());
 }
